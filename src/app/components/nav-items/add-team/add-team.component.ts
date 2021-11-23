a1477a8c48b825008743910b9abf89259dcb0157
Scratch29 <Jiamingw29@Gmail.com>
2021-11-23 15:55:04 -0500

    teams now submit team object to backend

--- src/app/components/nav-items/add-team/add-team.component.ts
+++ src/app/components/nav-items/add-team/add-team.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Team} from "../../../Team";
+import {TeamService} from "../../../services/team.service";
 
 @Component({
   selector: 'app-add-team',
@@ -9,13 +10,19 @@ import {Team} from "../../../Team";
 export class AddTeamComponent implements OnInit {
   team: Team = new Team();
 
-  constructor() {
+  constructor(private teamService: TeamService) {
   }
 
   ngOnInit(): void {
   }
 
   public onSubmit(): void {
+    if (this.team.teamName == undefined || this.team.teamNumber == undefined) {
+      alert("team name or number can not be empty");
+      return;
+    }
+    this.teamService.addTeam(this.team);
     console.log(this.team)
+    this.team = new Team();
   }
 }
