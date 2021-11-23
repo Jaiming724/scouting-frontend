d82258695d2bc02b87637a91fc3ac28c916440f4
Scratch29 <Jiamingw29@Gmail.com>
2021-11-23 15:53:42 -0500

    created team service

---
+++ src/app/services/team.service.ts
@@ -0,0 +1,43 @@
+import {Injectable} from '@angular/core';
+import {environment} from "../../environments/environment";
+import {Team} from "../Team";
+import {BehaviorSubject, Observable} from "rxjs";
+import {HttpClient} from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TeamService {
+  private apiUrl: string = environment.apiUrl;
+  private teams: Team[] = [];
+  private subject = new BehaviorSubject<Team[]>(this.teams);
+
+  constructor(private http: HttpClient) {
+    this.getTeams().subscribe((tasks) => {
+      this.teams = tasks
+      this.subject.next(this.teams)
+    });
+  }
+
+  private getTeams(): Observable<Team[]> {
+    return this.http.get<Team[]>(this.apiUrl + "getTeams");
+  }
+
+  public getTeamList(): Observable<Team[]> {
+    return this.subject.asObservable();
+  }
+
+  public addTeam(team: Team): void {
+    this.http.post<Team>(this.apiUrl + "addTeam", team).subscribe((newTeam) => {
+      this.teams.push(newTeam);
+      this.subject.next(this.teams)
+    })
+  }
+
+  public updateTeam(team: Team): void {
+
+    this.http.post<Team>(this.apiUrl + "addTeam", team).subscribe((newTeam) => {
+      this.subject.next(this.teams)
+    })
+  }
+}
